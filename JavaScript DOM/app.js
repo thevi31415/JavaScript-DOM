45a8079d774f90fd2bcd1d5d8771c7cab5df308a
thevi31415 <92256900+thevi31415@users.noreply.github.com>
2022-12-09 08:44:25 +0700

    first commit

---
+++ JavaScript DOM/app.js
@@ -0,0 +1,8 @@
+alert("Hello")
+let tagObject = document.getElementsByTagName('p')
+tagObject[0].style.color = 'Red'
+tagObject[2].style.backgroundColor ='Yellow'
+let listObject = document.getElementsByClassName('tag')
+let id = document.getElementById('one').innerHTML
+
+console.log(id)
